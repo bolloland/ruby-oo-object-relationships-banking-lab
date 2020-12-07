aa29829b0a963cb866bde0bab84518fb631427e9
ryan.bollettino@gmail.com <bollo@bollo-laptop.localdomain>
2020-12-06 18:30:24 -0600

    commit tree e36a68f3897acb0688720715f9f843e1d742328c

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,41 @@
+require 'pry'
 class Transfer
-  # your code here
+# pending-complete-rejected-reversed
+  attr_accessor :sender, :receiver, :status, :amount, :execute_transaction, :reverse_transfer
+  attr_writer
+  attr_reader 
+
+ def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+ end
+
+ def valid?  
+  (sender.valid? && receiver.valid?) ? true : false
+ end
+
+ def execute_transaction
+  if (@status != "pending") || !self.valid? || @sender.balance < self.amount 
+    @status = "rejected"
+    return "Transaction rejected. Please check your account balance."
+  else
+    @sender.balance -= self.amount
+    @receiver.balance += self.amount  
+    @status = "complete"
+  end
+  # !self.valid?
+end
+
+def reverse_transfer
+  #binding.pry
+  if self.status == "complete"
+        @sender.balance += self.amount
+        @receiver.balance -= self.amount
+        @status = "reversed"
+      else
+        @status = "rejected"
+      end
+  end
 end
